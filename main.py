3132ff6701996a48ebeb58e56e0c867333a20fa2
liborhavranek <liborhavranek91@gmail.com>
2022-08-04 23:17:02 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+
+response = requests.get("https://news.ycombinator.com/newest")
+
+status = response.status_code
+
+#print(response.text)
+
+yc_web_page = response.text
+
+soup = BeautifulSoup(yc_web_page, "html.parser")
+
+articles = soup.find_all(name="a", class_="titlelink")
+
+article_texts = []
+article_links = []
+
+for article_tag in articles:
+	text = article_tag.get_text()
+	article_texts.append(text)
+	link = article_tag.get("href")
+	article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0])for score in soup.find_all(name="span", class_ = "score")]
+
+# print(article_texts)
+# print(article_links)
+# print(article_upvotes)
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+
+print(article_texts[largest_index])
+print(article_links[largest_index])
